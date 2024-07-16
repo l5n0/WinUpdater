87e0c839cc4fa606109c6752374397600b359e37
Leandro <a@l5.lol>
2024-07-16 08:27:07 +0200

    20240716-0826
    Fast as fuck boiiiiii

--- UpdateManager/UpdateManagerApp/Components/HomeControl.cs
+++ UpdateManager/UpdateManagerApp/Components/HomeControl.cs
@@ -1,24 +1,27 @@
 using System;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using LiveCharts.Wpf;
 using System.Windows.Forms.Integration;
-using System.Windows.Media;
+using Brushes = System.Windows.Media.Brushes;
 
 namespace UpdateManagerApp
 {
     public partial class HomeControl : UserControl
     {
-        private Label lblApplications;
-        private ProgressBar applicationsProgressBar;
-        private Label lblDrivers;
-        private ProgressBar driversProgressBar;
-        private ElementHost elementHost;
-        private PieChart pieChart;
+        private Label lblApplications = null!;
+        private ProgressBar applicationsProgressBar = null!;
+        private Label lblDrivers = null!;
+        private ProgressBar driversProgressBar = null!;
+        private ElementHost elementHostApplications = null!;
+        private ElementHost elementHostDrivers = null!;
+        private PieChart pieChartApplications = null!;
+        private PieChart pieChartDrivers = null!;
 
         public HomeControl()
         {
             InitializeComponent();
-            LoadChart();
+            LoadChartsAsync();
         }
 
         private void InitializeComponent()
@@ -27,8 +30,10 @@ namespace UpdateManagerApp
             this.applicationsProgressBar = new ProgressBar();
             this.lblDrivers = new Label();
             this.driversProgressBar = new ProgressBar();
-            this.elementHost = new ElementHost();
-            this.pieChart = new PieChart();
+            this.elementHostApplications = new ElementHost();
+            this.elementHostDrivers = new ElementHost();
+            this.pieChartApplications = new PieChart();
+            this.pieChartDrivers = new PieChart();
 
             // Labels and ProgressBars setup
             this.lblApplications.Text = "Applications Up-to-date:";
@@ -39,21 +44,41 @@ namespace UpdateManagerApp
             this.lblDrivers.Dock = DockStyle.Top;
             this.driversProgressBar.Dock = DockStyle.Top;
 
-            // ElementHost setup
-            this.elementHost.Dock = DockStyle.Fill;
-            this.elementHost.Child = this.pieChart;
+            // Set PieChart size
+            int pieChartSize = 300;
+
+            this.pieChartApplications.Width = pieChartSize;
+            this.pieChartApplications.Height = pieChartSize;
+
+            this.pieChartDrivers.Width = pieChartSize;
+            this.pieChartDrivers.Height = pieChartSize;
+
+            // ElementHosts setup
+            this.elementHostApplications.Dock = DockStyle.Top;
+            this.elementHostApplications.Child = this.pieChartApplications;
+            
+            this.elementHostDrivers.Dock = DockStyle.Fill;
+            this.elementHostDrivers.Child = this.pieChartDrivers;
 
             // Layout
-            this.Controls.Add(this.elementHost);
+            this.Controls.Add(this.elementHostDrivers);
             this.Controls.Add(this.driversProgressBar);
             this.Controls.Add(this.lblDrivers);
+            this.Controls.Add(this.elementHostApplications);
             this.Controls.Add(this.applicationsProgressBar);
             this.Controls.Add(this.lblApplications);
         }
 
-        private void LoadChart()
+        private async void LoadChartsAsync()
         {
-            // Simulated data
+            await Task.Run(() => LoadData());
+            UpdateUI();
+        }
+
+        private (int totalApplications, int upToDateApplications, int outOfDateApplications,
+                 int totalDrivers, int upToDateDrivers, int outOfDateDrivers) LoadData()
+        {
+            // Simulated data loading
             int totalApplications = 100;
             int upToDateApplications = 75;
             int outOfDateApplications = totalApplications - upToDateApplications;
@@ -62,20 +87,33 @@ namespace UpdateManagerApp
             int upToDateDrivers = 45;
             int outOfDateDrivers = totalDrivers - upToDateDrivers;
 
+            return (totalApplications, upToDateApplications, outOfDateApplications,
+                    totalDrivers, upToDateDrivers, outOfDateDrivers);
+        }
+
+        private void UpdateUI()
+        {
+            var data = LoadData();
+
             // Update ProgressBars
-            this.applicationsProgressBar.Maximum = totalApplications;
-            this.applicationsProgressBar.Value = upToDateApplications;
+            this.applicationsProgressBar.Maximum = data.totalApplications;
+            this.applicationsProgressBar.Value = data.upToDateApplications;
 
-            this.driversProgressBar.Maximum = totalDrivers;
-            this.driversProgressBar.Value = upToDateDrivers;
+            this.driversProgressBar.Maximum = data.totalDrivers;
+            this.driversProgressBar.Value = data.upToDateDrivers;
 
-            // Update PieChart
-            this.pieChart.Series = new LiveCharts.SeriesCollection
+            // Update PieChart for Applications
+            this.pieChartApplications.Series = new LiveCharts.SeriesCollection
             {
-                new PieSeries { Title = "Up-to-date Applications", Values = new LiveCharts.ChartValues<int> { upToDateApplications }, Fill = System.Windows.Media.Brushes.Green },
-                new PieSeries { Title = "Out-of-date Applications", Values = new LiveCharts.ChartValues<int> { outOfDateApplications }, Fill = System.Windows.Media.Brushes.Red },
-                new PieSeries { Title = "Up-to-date Drivers", Values = new LiveCharts.ChartValues<int> { upToDateDrivers }, Fill = System.Windows.Media.Brushes.Blue },
-                new PieSeries { Title = "Out-of-date Drivers", Values = new LiveCharts.ChartValues<int> { outOfDateDrivers }, Fill = System.Windows.Media.Brushes.Orange }
+                new PieSeries { Title = "Up-to-date Applications", Values = new LiveCharts.ChartValues<int> { data.upToDateApplications }, Fill = Brushes.Green },
+                new PieSeries { Title = "Out-of-date Applications", Values = new LiveCharts.ChartValues<int> { data.outOfDateApplications }, Fill = Brushes.Red }
+            };
+
+            // Update PieChart for Drivers
+            this.pieChartDrivers.Series = new LiveCharts.SeriesCollection
+            {
+                new PieSeries { Title = "Up-to-date Drivers", Values = new LiveCharts.ChartValues<int> { data.upToDateDrivers }, Fill = Brushes.Blue },
+                new PieSeries { Title = "Out-of-date Drivers", Values = new LiveCharts.ChartValues<int> { data.outOfDateDrivers }, Fill = Brushes.Orange }
             };
         }
     }
---
+++ UpdateManager/UpdateManagerApp/Components/MainForm.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Windows.Forms;
+
+namespace UpdateManagerApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnHome_Click(object sender, EventArgs e)
+        {
+            LoadControl(new HomeControl());
+        }
+
+        private void BtnUpdateApplications_Click(object sender, EventArgs e)
+        {
+            LoadControl(new UpdateApplicationsControl());
+        }
+
+        private void BtnUpdateDrivers_Click(object sender, EventArgs e)
+        {
+            LoadControl(new UpdateDriversControl());
+        }
+
+        private void BtnDiskUsage_Click(object sender, EventArgs e)
+        {
+            LoadControl(new DiskUsageControl());
+        }
+
+        private void LoadControl(UserControl control)
+        {
+            this.contentPanel.Controls.Clear();
+            control.Dock = DockStyle.Fill;
+            this.contentPanel.Controls.Add(control);
+        }
+    }
+}
